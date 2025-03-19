7046c6ae10843800e3d69fa54d21efa54618f95b
dc0x13 <juan.patino.dc@gmail.com>
2025-03-19 00:18:17 -0500

    ok

--- argxs.c
+++ argxs.c
@@ -8,16 +8,28 @@
  */
 #include "argxs.h"
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define TERM_OPT_UNSEEN 0
-#define TERM_OPT_SEEN   1
+#define TERM_OPT_UNSEEN     0
+#define TERM_OPT_SEEN       1
 
 static unsigned char __term_opt_is = TERM_OPT_UNSEEN;
 
+enum argv_kind
+{
+    argvs_long_flag,
+    argvs_shrt_flag,
+    argvs_argument,
+    argvs_endopmkr,
+};
 
 static void* gotta_realloc (const unsigned int, unsigned int*, void**, const size_t);
+static enum argv_kind what_is_this (const char*);
+
+static void long_flag (const struct argxs_flag*, struct argxs_seen*, const char*);
 
 struct argxs_parsed *argxs (const int argc, char **argv, const struct argxs_flag *flags)
 {
@@ -42,6 +54,15 @@ struct argxs_parsed *argxs (const int argc, char **argv, const struct argxs_flag
             ps->posargs[ps->no_parg++] = argv[i];
             continue;
         }
+
+        switch (what_is_this(argv[i]))
+        {
+            case argvs_long_flag:
+            case argvs_shrt_flag:
+            {
+                break;
+            }
+        }
     }
 
     return ps;
@@ -61,4 +82,30 @@ static void* gotta_realloc (const unsigned int ln, unsigned int *cp, void **pt, 
 
     if (*pt == NULL) { abort(); }
     return *pt;
+}
+
+static enum argv_kind what_is_this (const char *thing)
+{
+    if ((*thing == '-') && (thing[1] == '-'))
+    {
+        if (thing[2] == '\0') return argvs_endopmkr;
+        if (isalnum(thing[2])) return argvs_long_flag;
+        return argvs_error;
+    }
+
+    if ((*thing == '-') && isalnum(thing[1]))
+    {
+        return argvs_shrt_flag;
+    }
+
+    return argvs_argument;
+}
+
+static void long_flag (const struct argxs_flag *flags, struct argxs_seen *seen, const char *name)
+{
+    // 1. get until you find a = (if any)
+    // 2. compare this length against the precomputed already
+    for (int i = 0; flags[i].name != NULL; i++)
+    {
+    }
 }
